3bdcc237c80775091f3405743172f49482c5f5c7
Dmitry Shechtman <damnian@gmail.com>
2017-05-02 16:39:05 +0300

    Add solution

---
+++ src/Net.Chdk.Providers.Crypto/HashProvider.cs
@@ -0,0 +1,27 @@
+﻿using System.IO;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace Net.Chdk.Providers.Crypto
+{
+    sealed class HashProvider : IHashProvider
+    {
+        public string GetHashString(string filePath, string hashName)
+        {
+            var hash = ComputeHash(filePath, hashName);
+            var sb = new StringBuilder(hash.Length * 2);
+            for (int i = 0; i < hash.Length; i++)
+                sb.Append(hash[i].ToString("x2"));
+            return sb.ToString();
+        }
+
+        private static byte[] ComputeHash(string filePath, string hashName)
+        {
+            var hashAlgorithm = HashAlgorithm.Create(hashName);
+            using (var stream = File.OpenRead(filePath))
+            {
+                return hashAlgorithm.ComputeHash(stream);
+            }
+        }
+    }
+}
