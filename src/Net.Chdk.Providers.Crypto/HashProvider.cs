33da5eb1d3d7c9b1e2cc053e0fa99ed662e50639
Dmitry Shechtman <damnian@gmail.com>
2017-05-11 18:40:49 +0300

    Remove GetHashString(string, string)

--- src/Net.Chdk.Providers.Crypto/HashProvider.cs
+++ src/Net.Chdk.Providers.Crypto/HashProvider.cs
@@ -6,14 +6,6 @@ namespace Net.Chdk.Providers.Crypto
 {
     sealed class HashProvider : IHashProvider
     {
-        public string GetHashString(string filePath, string hashName)
-        {
-            using (var stream = File.OpenRead(filePath))
-            {
-                return GetHashString(stream, hashName);
-            }
-        }
-
         public string GetHashString(Stream stream, string hashName)
         {
             var hash = ComputeHash(stream, hashName);
