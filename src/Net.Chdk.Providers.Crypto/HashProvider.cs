3da74998a38a942b1b040468246ed778dc1edd89
Dmitry Shechtman <damnian@gmail.com>
2017-07-12 16:26:33 +0300

    Retarget to .NET Standard 1.3

--- src/Net.Chdk.Providers.Crypto/HashProvider.cs
+++ src/Net.Chdk.Providers.Crypto/HashProvider.cs
@@ -1,4 +1,5 @@
-﻿using System.IO;
+﻿using System;
+using System.IO;
 using System.Security.Cryptography;
 using System.Text;
 
@@ -28,14 +29,33 @@ namespace Net.Chdk.Providers.Crypto
 
         private static byte[] ComputeHash(Stream stream, string hashName)
         {
-            return HashAlgorithm.Create(hashName)
+            return CreateHashAlgorithm(hashName)
                 .ComputeHash(stream);
         }
 
         private static byte[] ComputeHash(byte[] buffer, string hashName)
         {
-            return HashAlgorithm.Create(hashName)
+            return CreateHashAlgorithm(hashName)
                 .ComputeHash(buffer);
+        }
+
+        private static HashAlgorithm CreateHashAlgorithm(string hashName)
+        {
+            switch (hashName)
+            {
+                case "md5":
+                    return MD5.Create();
+                case "sha1":
+                    return SHA1.Create();
+                case "sha256":
+                    return SHA256.Create();
+                case "sha384":
+                    return SHA384.Create();
+                case "sha512":
+                    return SHA512.Create();
+                default:
+                    throw new ArgumentException("Invalid hash name");
+            }
         }
     }
 }
